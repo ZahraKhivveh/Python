9ac71c6bcbb66c48a5d47373054eb7d894e83d33
zahra khivveh <zahrakhivveh@gmail.com>
2026-06-27 15:39:40 +0330

    first-commit-04

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ x = 7 + 8  +\
     10 - 2;
 print(x)
 
-print(1,2,3,4,5 , sep= " or ");
+help("keywords");
+print(1,2,3,4,5 , sep= " and ");
 print(1,2,3,4,5 , end= " hi ");
-o = input("what is your name?");
-print("hi to " , o)
+# o = input("what is your name?");
+# print("hi to " , o)
+a = 20 ; b = 10; c = 3;
+print( a , b , c, end=" ") ; print( a!=a)
+print( a == a)
+print( a > a)
+print(a + b )
+print(a - b )
+print(a * b )
+print(a / b )
+print(a // b )
+print(a % b )
+print(a ** c)
+
+
+
