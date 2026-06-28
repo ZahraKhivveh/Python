c6f2abbe206584d912e981b453dc3d46f4011987
zahra khivveh <zahrakhivveh@gmail.com>
2026-06-28 22:59:09 +0330

    first-commit-05

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ print(1,2,3,4,5 , end= " hi ");
 # print("hi to " , o)
 a = 20 ; b = 10; c = 3;
 print( a , b , c, end=" ") ; print( a!=a)
-print( a == a)
+print( a == a , end=" ") ;
 print( a > a)
 print(a + b )
 print(a - b )
@@ -18,6 +18,12 @@ print(a / b )
 print(a // b )
 print(a % b )
 print(a ** c)
+print(not a==a );
+u =[1, 2 , 3];
+w= [ 1 , 2 ,3];
+print(3 not in u)
+print(u is not w)
+print( (d:=6) > 0 )
 
 
 
